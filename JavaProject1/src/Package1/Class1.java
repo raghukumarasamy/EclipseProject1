ff5eb31cc3314cf63b55bd1405b9962cfd77185d
RKumarasamy <RKumarasamy@ACHEINLT0019982.ashleyfurniture.com>
2021-08-06 11:32:25 +0530

    1st commit

---
+++ JavaProject1/src/Package1/Class1.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SUGI");
+		System.out.println("RAGHU");
+
+	}
+
+}
